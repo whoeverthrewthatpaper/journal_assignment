ad79724f05866b463ae5866b23768bd49fbf9de6
whoeverthrewthatpaper <75176559+whoeverthrewthatpaper@users.noreply.github.com>
2021-02-18 22:05:46 +0100

    Add files via upload

---
+++ journal_assignment/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+
+namespace journal_assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+            string Date = DateTime.Today.ToString("D");
+
+            Intro();
+
+            while (true)
+            {
+                input = Console.ReadLine();
+                if (input == "start" || input == "Start" || input == "START") 
+                    break;
+
+
+            }
+            using (StreamWriter Journal = new StreamWriter(Date + ".txt", true))
+            {
+                Console.Clear();
+                Console.WriteLine("Start writing");
+
+                while (true)
+                {
+                    input = Console.ReadLine();
+                    if (input == "stop" || input== "Stop" ||input== "STOP") break;
+                    Journal.WriteLine(input);
+                    Journal.Flush();
+
+                }
+            }
+            Exit();
+
+        }
+        static void Intro()
+        {
+            Console.WriteLine(" Let's make a journal!");
+            Console.WriteLine("type 'start' to begin writing");
+            Console.WriteLine("type 'stop' to stop writing and save");
+
+        }
+        static void Exit ()
+        {
+            Console.Clear();
+            Console.WriteLine("Push any botton to restart");
+            Console.ReadKey();
+        }
+    }
+}
